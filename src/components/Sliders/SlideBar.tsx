88909ceb030a959dfac63afbe93b48c5d5f7dd5b
Omsai Official <omsaix100gmail.com>
2024-06-03 23:35:43 +0530

    feat : added logic for thumb dragging it's still work in progress.

--- src/components/Sliders/SlideBar.tsx
+++ src/components/Sliders/SlideBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import './SliderBar.css'
 
 interface SliderProps {
@@ -25,9 +25,11 @@ const labels = [
 const SliderBar = (props: SliderProps) => {
     const { start = 0, end = 100, defaultValue = 0, step = 1, fill, background, onChange, discrete = false } = props
     const [value, setValue] = useState(defaultValue)
+    const sliderRef = useRef<HTMLDivElement>(null)
+    console.log(value, 'value', typeof value);
 
     const onSlide = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const newValue = Number(event.target.value);
+        const newValue = Math.round(Number(event.target.value));
         setValue(newValue)
         if (onChange) {
             onChange(newValue)
@@ -47,7 +49,7 @@ const SliderBar = (props: SliderProps) => {
     const labelsMapper = labels.map((item: string, index: number) => {
         const labelPosition = (100 / (labels.length - 1)) * index;
         return (
-            <span className='SliderBar-labels' style={{ left: `${labelPosition}%` }}>
+            <span key={index} className='SliderBar-labels' style={{ left: `${labelPosition}%` }}>
                 {item}
             </span>
         )
@@ -64,6 +66,48 @@ const SliderBar = (props: SliderProps) => {
     const stepCalculator = (discrete ? (100 / (labels.length - 1)) : step).toFixed(2);
     // console.log('stepCalculator', stepCalculator, typeof stepCalculator);
 
+    const handleMouseDown = (e: React.MouseEvent) => {
+
+        const handleMouseMove = (e: MouseEvent) => {
+            if (!sliderRef.current) {
+                return;
+            }
+            const slider = sliderRef.current;
+            const rect = slider.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const width = rect.right - rect.left;
+            let newValue = ((x / width) * (end - start)) + start;
+
+            // Calculate the nearest step
+            newValue = Math.round((newValue / step) * step);
+
+            //Ensure the value is within the range
+            newValue = Math.max(start, Math.min(end, newValue));
+
+            setValue(newValue);
+            if (onChange) {
+                onChange(newValue);
+            }
+
+            console.log('newValue Mouse Event', newValue);
+        }
+
+        const handleMouseUp = () => {
+            document.removeEventListener('mousemove', handleMouseMove);
+            document.removeEventListener('mouseup', handleMouseUp);
+        }
+
+        // Remove the old event listeners
+        document.removeEventListener('mousemove', handleMouseMove);
+        document.removeEventListener('mouseup', handleMouseUp);
+
+        // Add new event listeners
+        document.addEventListener('mousemove', handleMouseMove);
+        document.addEventListener('mouseup', handleMouseUp);
+        e.preventDefault(); // Prevent any default action
+
+    }
+
 
     return (
         <div className='SliderBar-Wrapper'>
@@ -79,7 +123,7 @@ const SliderBar = (props: SliderProps) => {
                     step={stepCalculator}
                 />
                 <div className='Slider-custom-thumb-wrapper'>
-                    <span className='Slider-custom-thumb' style={customThumbPositionLogic()}></span>
+                    <span className='Slider-custom-thumb' style={customThumbPositionLogic()} onMouseDown={handleMouseDown}></span>
                 </div>
                 <div className="SliderBar-label-container">
                     {labelsMapper}
