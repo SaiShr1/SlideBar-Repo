70f89f095f4fc930e531aa7ec9898c8243a8dd3c
Omsai Official <omsaix100gmail.com>
2024-06-08 21:11:45 +0530

    chore: "Made slider homepage ui bit more pleasing."

--- src/App.tsx
+++ src/App.tsx
@@ -38,29 +38,34 @@ function App() {
   ]
 
   return (
-    <>
-      <h3>Discrete Slider</h3>
-      <SliderBar
-        defaultValue={0}
-        discrete={true}
-        labelsDescriptionArray={labelsDescriptionArray}
-        // labelsDescriptionArray={[]}
-        marks={true}
-      // start={0}
-      // end={1}
-      // step={-1}
-      />
-      <h3>Slider</h3>
-      <SliderBar
-        // step={10}
-        defaultValue={0}
-        discrete={false}
-        labelsDescriptionArray={labelsDescriptionArrayShort}
-        marks={true}
-        markForEachStep={true}
-        showValue={true}
-      />
-    </>
+    <div className='Slider'>
+      <div className='Slider__discrete'>
+        <h3>Discrete Slider</h3>
+        <SliderBar
+          defaultValue={0}
+          discrete={true}
+          labelsDescriptionArray={labelsDescriptionArray}
+          // labelsDescriptionArray={[]}
+          marks={true}
+        // start={0}
+        // end={1}
+        // step={-1}
+        />
+      </div>
+      <div className='Slider__smooth'>
+
+        <h3>Smooth Slider With Value ToolTip!</h3>
+        <SliderBar
+          // step={10}
+          defaultValue={0}
+          discrete={false}
+          labelsDescriptionArray={labelsDescriptionArrayShort}
+          marks={false}
+          markForEachStep={true}
+          showValue={true}
+        />
+      </div>
+    </div>
   )
 }
 
